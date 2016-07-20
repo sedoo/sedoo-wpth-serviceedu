64005b7bbe16cf9568c3a21d2b8565334ca0ba55
Pierre Vert <vert@obs-mip.fr>
2016-07-20 13:37:55 +0200

    test commit

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
+ * OMP
  * @package portail_service_educatif
  */
 
